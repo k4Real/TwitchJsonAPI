ec9affc623c6c05ff6df31c238e9e02189037a6f
Karels Jacques <karelsjacques@gmail.com>
2017-08-27 13:30:48 +0200

    done
    just needs some finishing, cleaning up code.

--- TwitchJsonAPI/public_html/js/twitchApp.js
+++ TwitchJsonAPI/public_html/js/twitchApp.js
@@ -11,14 +11,15 @@
  * searhcbar
  * 
  */
-
+var streamer="" ;
+var temp;
 var name ;
 var link;
 var status ;
 var game;
 var icon ;
 var thumbnail ;
-var streamerList = ["OgamingSC2", "OgamingSC2", "cretetion", "freecodecamp", "cohhcarnage", "awkwards_travel", "RobotCaleb", "pokket"] ;
+var streamerList = ["reckful", "OgamingSC2", "cretetion", "freecodecamp", "noopkat", "awkwards_travel", "RobotCaleb", "pokket"] ;
 var twitchAPI = "https://wind-bow.gomix.me/twitch-api/streams/" ;
 var arrayStreamer= [] ;
 
@@ -26,8 +27,19 @@ var arrayStreamer= [] ;
 
 //*************************
 
-function streamerListSet (response){
+function onlineList (response){
+    
+
+   
+    if(response.stream !== null)
+        
+    {
+        
+    
+    
     var divAppend;
+    
+    
   game =response.stream.game;
   thumbnail=response.stream.preview.medium;
   status = response.stream.channel.status ;
@@ -35,21 +47,79 @@ function streamerListSet (response){
   icon = response.stream.channel.logo ;
   link =response.stream.channel.url ;
   
-  divAppend= "<div class='streamer'><img src="+icon +" class='streamerIcon' alt='streamer Icon'>  <a href="+link+" class='streamerName'>"+name+"</a></div>";
+  divAppend= "<div class='streamer'><img src="+icon +" class='streamerIcon' alt='streamer Icon'>  <a href="+link+" class='streamerName'>"+name+"</a><img src="+thumbnail+ " class='previewPic' alt='PreviewPic'><p class='streamerDescription'>"+status+"</p><p class='game'>"+game+"</p><p class='status'>online  <span class='onlineIcon'></span></p></div>";
   
   $("#streamerList").append(divAppend);
-  
+    }
     
 };
 
- function toHTML (){
-     
-     
-     
-     
-     
- } ;
+ //*****************************************
+ 
+ function offlineList (response){
+    
 
+   
+  
+        
+    
+    
+    var divAppend;
+    
+    
+  game =response.game;
+  
+  status = response.status ;
+  name =response.display_name  ;
+  icon = response.logo ;
+  link =response.url ;
+  
+  divAppend= "<div class='streamer'><img src="+icon +" class='streamerIcon' alt='streamer Icon'>  <a href="+link+" class='streamerName'>"+name+"</a><p class='status'>offline  <span class='offlineIcon'></span></p></div>";
+  
+  $("#streamerList").append(divAppend);
+    
+    
+};
+ 
+//*******************************************
+
+function allList (response){
+    
+
+   
+    if(response.stream !== null)
+        
+    {
+        
+    
+    
+    var divAppend;
+    
+    
+  game =response.stream.game;
+  thumbnail=response.stream.preview.medium;
+  status = response.stream.channel.status ;
+  name =response.stream.channel.display_name  ;
+  icon = response.stream.channel.logo ;
+  link =response.stream.channel.url ;
+  
+  divAppend= "<div class='streamer'><img src="+icon +" class='streamerIcon' alt='streamer Icon'>  <a href="+link+" class='streamerName'>"+name+"</a><img src="+thumbnail+ " class='previewPic' alt='PreviewPic'><p class='streamerDescription'>"+status+"</p><p class='game'>"+game+"</p><p class='status'>online  <span class='onlineIcon'></span></p></div>";
+  
+  $("#streamerList").append(divAppend);
+    }
+   
+    else {
+           temp =response._links.channel ;
+        streamer= temp.replace('https://api.twitch.tv/kraken/channels/', '');
+        let twitchAPI = "https://wind-bow.gomix.me/twitch-api/channels/" ;
+        twitchAPI += streamer+"?callback=?" ;
+        
+        jQuery.getJSON( twitchAPI  ,offlineList  ) ;
+        
+    }
+    
+    
+};
 
 
 
@@ -57,24 +127,20 @@ function streamerListSet (response){
 //***************************************
 
 
-function updateAll() {
+function updateOnline() {
     
     //https://forum.freecodecamp.org/t/twitch-tv-api-challenge-cross-origin-issue/66217/2
+       $("#streamerList").empty(); 
     
-    for( let i= 0; i<2; i++) {
-        
+    for( let i= 0; i<streamerList.length; i++) {
+   
         let twitchAPI = "https://wind-bow.gomix.me/twitch-api/streams/" ;
         twitchAPI += streamerList[i]+"?callback=?" ;
         
-        jQuery.getJSON( twitchAPI  ,streamerListSet  ) ;
+        jQuery.getJSON( twitchAPI  , onlineList  ) ;
     }
     
     
-
-
-
-
-
 
 
 //$.ajax({
@@ -92,6 +158,99 @@ function updateAll() {
 
 }
 
+function updateAll() {
+    
+    //https://forum.freecodecamp.org/t/twitch-tv-api-challenge-cross-origin-issue/66217/2
+       $("#streamerList").empty(); 
+    
+    for( let i= 0; i<streamerList.length; i++) {
+               
+               
+        let twitchAPI = "https://wind-bow.gomix.me/twitch-api/streams/" ;
+        twitchAPI += streamerList[i]+"?callback=?" ;
+        
+        jQuery.getJSON( twitchAPI  ,allList  ) ;
+        
+    }
+    
+}
+//***************************************************
+
+function updateOffline () {
+    
+    
+    $("#streamerList").empty(); 
+    
+    for( let i= 0; i<streamerList.length; i++) {
+               
+               
+        let twitchAPI = "https://wind-bow.gomix.me/twitch-api/streams/" ;
+        twitchAPI += streamerList[i]+"?callback=?" ;
+        
+        jQuery.getJSON( twitchAPI  ,offlineOnly  ) ;
+        
+    }
+    
+    
+    
+    
+}
+//****************************************************
+
+function offlineOnly (response) {
+    
+    
+        
+    if(response.stream === null)
+        
+    {
+        
+      temp =response._links.channel ;
+        streamer= temp.replace('https://api.twitch.tv/kraken/channels/', '');
+        let twitchAPI = "https://wind-bow.gomix.me/twitch-api/channels/" ;
+        twitchAPI += streamer+"?callback=?" ;
+        
+        jQuery.getJSON( twitchAPI  ,offlineList  ) ;
+    }
+   
+  
+    
+    
+    
+}
+
+
+
+
+
+
+
 //*****************************************
 
- updateAll() ;
+
+
+$('#online').on('click',updateOnline);
+$('#all').on('click',updateAll);
+$('#offline').on('click',updateOffline);
+
+
+ $("#searchBox").keypress(function(event){
+     $("#streamerList").empty(); 
+    if (event.key==='Enter')
+    {
+         searchText = this.value;
+         
+          let twitchAPI = "https://wind-bow.gomix.me/twitch-api/streams/" ;
+        twitchAPI += searchText+"?callback=?" ;
+        
+        jQuery.getJSON( twitchAPI  ,allList  ) ;
+        
+         
+      
+  }
+  
+ }) ;
+
+
+ 
+ 
